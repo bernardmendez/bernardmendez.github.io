43806a2066107df343fbf93c04b8c65f8c3e3f94
Bernard <56208067+bernardmendez@users.noreply.github.com>
2020-01-02 15:19:57 -0800

    Minor fixes

---
+++ js/posts/projects.js
@@ -0,0 +1,18 @@
+//1
+
+var websitev1 = {
+title: 'Website Version One Launched',
+author: '',
+url: '',
+image: 'url("images/website.jpg")',
+text: '',
+};
+
+var empty = {
+title: '',
+author: '',
+url: '',
+image: '',
+text: '',
+}
+
