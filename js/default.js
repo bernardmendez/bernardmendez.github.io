b9ec17716690837d78868d9dde9256509b4477e2
bernardmendez <bernard.h.mendez@gmail.com>
2020-01-12 12:07:02 -0800

    Major design changes

--- js/default.js
+++ js/default.js
@@ -1,17 +1,16 @@
 var date = new Date();
 
-daynamelist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-monthlist = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var daynamelist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var monthlist = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-var daynumber = date.getDay() - 2;
-var dayname = daynamelist[daynumber];
+var daynumber = date.getDate();
+//var dayname = daynamelist[daynumber];
 var month = monthlist[date.getMonth()];
 var year = date.getFullYear();
-var fulldate = dayname + ' ' + month + ' ' + daynumber + ', ' + year;
+var fulldate = month + ' ' + daynumber + ', ' + year;
+var header = "<div id='topbar'><div id='home'><div id='date'></div><div id='name'><a href='index.html'>Bernard Mendez</a></div><div class='mobile2 links'><a href='about.html'>About</a><a href='contact.html'>Contact</a><a href='employers.html'>Employers</a></div></div></div><div id='extras'><a href='favourites.html'>Favourites</a><a href='projects.html'>Projects</a><a href='publications.html'>Publications</a></div>";
 
-var header = "<div id='date'></div><div id='topbar'><div id='home'><a href='index.html' id='name'>bernard<br>mendez</a><div id='links'><a href='about.html'>About</a><a href='contact.html'>Contact</a><a href='employers.html'>Employers</a></div></div></div><div id='extras'><a href='favourites.html'>favourites</a><a href='projects.html'>projects</a><a href='publications.html'>publications</a></div>";
-
-var entry = "<a class='article' onclick='newtab(this.value)' href='javascript:void(0)'><div class='description'><div class='title'>title</div><div class='line0'></div><div class='author'>author</div><div class='summary'>summary</div></div><div class='img'></div></a>";
+var entry = "<a class='article' onclick='newtab(this)' href='javascript:void(0)'><div class='description'><div class='title'>title</div><div class='author'>author</div><div class='summary'>summary</div></div><div class='img'></div></a>";
 
 var apnews = 'https://apnews.com';
 var hodinkee = 'https://www.hodinkee.com';
@@ -26,7 +25,13 @@ var atwentry = "<a class='atw img' onclick='newtab(value)' href='javascript:void
 
 
 function newtab(link) {
-setTimeout(function() {
-window.open(link, '_blank')
-}, 10);
+//link.getElementsByClassName('highlight')[0];
+var temp = link.getElementsByClassName('highlight')[0]
+temp.style.backgroundPosition = 'left top';
+temp.style.color = 'black';
+setTimeout(function(){
+window.open(link.value, '_blank')
+temp.style.backgroundPosition = 'right bottom';
+           }, 1000)
 }
+
--- js/posts/beatles.js
+++ js/posts/beatles.js
@@ -2,20 +2,20 @@
 
 var album0 = ['I Saw Her Standing There', 'Misery', 'Anna (Go To Him)', 'Chains', 'Boys', 'Ask Me Why', 'Please Please Me', 'Love Me Do', 'PS I Love You', 'Baby It’s You', 'Do You Want To Know A Secret', 'A Taste Of Honey', 'There’s A Place', 'Twist And Shout'];
 
-var album1 = 
+var album1 = '';
 
-var please = ['Please Please Me', 1963, album0];
-var with_the = ['With the Beatles', 1963];
-var hard = ["A Hard Day's Night", 1964];
-var sale = ['Beatles For Sale', 1964];
-var help = ['Help!', 1965];
-var rubber = ['Rubber Soul', 1965];
-var revolver = ['Revolver', 1966];
-var peppers = ["Sgt. Pepper's Lonely Hearts Club Band", 1967];
-var magical = ['Magical Mystery Tour', 1967];
-var white = ['The White Album', 1968];
-var yellow = ['The Yellow Submarine', 1969];
-var abbey = ['Abbey Road', 1969];
-var let_it = ['Let It Be', 1970];
+var please = {album: 'Please Please Me', year: 1963, image: 'url("images/beatles/please.jpg")'};
+var with_the = {album: 'With the Beatles', year: 1963, image: 'url("images/beatles/with.jpg")'};
+var hard = {album: "A Hard Day's Night", 1964, image: 'url("images/beatles/hard.jpg")'};
+var sale = {album: 'Beatles For Sale', year: 1964, image: 'url("images/beatles/sale.jpg")'};
+var help = {album: 'Help!', year: 1965, image: 'url("images/beatles/help.jpg")'};
+var rubber = {album: 'Rubber Soul', year: 1965, image: 'url("images/beatles/rubber.jpg")'};
+var revolver = {album: 'Revolver', year: 1966, image: 'url("images/beatles/revolver.jpg")'};
+var peppers = {"Sgt. Pepper's Lonely Hearts Club Band", 1967, image: 'url("images/beatles/peppers.jpg")'};
+var magical = {album: 'Magical Mystery Tour', year: 1967, image: 'url("images/beatles/magical.jpg")'};
+var white = {album: 'The White Album', year: 1968, image: 'url("images/beatles/white.jpg")'};
+var yellow = {album: 'The Yellow Submarine', year: 1969, image: 'url("images/beatles/yellow.jpg")'};
+var abbey = {album: 'Abbey Road', year: 1969, image: 'url("images/beatles/abbey.jpg")'};
+var let_it = {album: 'Let It Be', year: 1970, image: 'url("images/beatles/let.jpg")'};
 
 var albums = [please, with_the, hard, sale, help, rubber, revolver, peppers, magical, white, yellow, abbey, let_it];
