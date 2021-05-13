80371b5829961e714390542f27450949310205ec
bernardmendez <bernard.h.mendez@gmail.com>
2021-05-13 13:00:11 -0700

    simplified website for future redesign

--- src/homepage/About.js
+++ src/homepage/About.js
@@ -9,42 +9,32 @@ function About() {
      <div id='meat'>
      
        <h2>About Bernard</h2>
-       <ul>
-        <li>Bernard is a journalist based out of Los Angeles. He is currently the News editor at the Daily Bruin, the independent student newspaper at UCLA, and a third-year Applied Mathematics student. He is also a contributor for CalMatters as part of its College Journalism Network. Bernard enjoys reporting on positions of power, creating data visualizations and playing with front-end web development. In his free time, Bernard enjoys following the Buffalo Bills and tracking the vintage wristwatch market. You can contact Bernard at contact@bernardmendez.com.</li>
-       </ul>
-
+        <p>
+          Bernard is a journalist based out of Los Angeles. He currently attends UCLA, where he studies applied mathematics.
+        </p>
        <h2>Quick Links</h2>
        <ul>
-        <li><a href='https://twitter.com/bernardhmendez'>Twitter &#10063;</a></li>
-        <li><a href='https://www.linkedin.com/in/bernard-mendez/'>LinkedIn &#10063;</a></li>
-        <li><a href='https://github.com/bernardmendez'>Github &#10063;</a></li>
-        <li><a href='/Resume.pdf'>Resume &#10063;</a></li>
+        <li><a href='https://twitter.com/bernardhmendez'>Twitter &#10063;</a> – (@bernardhmendez)</li>
+        <li><a href='https://www.linkedin.com/in/bernard-mendez/'>LinkedIn &#10063;</a> – (/in/bernard-mendez)</li>
+        <li><a href='https://github.com/bernardmendez'>Github &#10063;</a> – (@bernardmendez)</li>
        </ul>
 
        <h2>Experience</h2>
        <ul>
-        <li>News editor, <a href='https://dailybruin.com'>Daily Bruin &#10063;</a> - <i>present</i></li>
-        <li>Contributor, <a href='https://calmatters.org'>CalMatters &#10063;</a> - <i>present</i></li>
-        <li>Data Journalist, <a href='https://stack.dailybruin.com'>The Stack &#10063;</a> - <i>2019 to 2020</i></li>
-        <li>Staff reporter, <a href='https://dailybruin.com'>Daily Bruin &#10063;</a> - <i>2019 to 2020</i></li>
-       </ul>
-
-       <h2>Freelance Reporting</h2>
-       <ul>
-        <li>Contact Bernard at bernardhmendez@protonmail.com</li>
+        <li>Editorial Intern, <a href='https://dot.LA'>dot.LA &#10063;</a> - <i>Summer 2021</i></li>
+        <li>News editor, <a href='https://dailybruin.com'>Daily Bruin &#10063;</a> - <i>2020 to 2021</i></li>
+        <li>Reporter/Photographer, <a href='https://dailybruin.com'>Daily Bruin &#10063;</a> - <i>2019 to 2020</i></li>
        </ul>
 
        <h2>Coding</h2>
        <ul>
-        <li>Object-oriented: <a href='https://www.python.org'>Python &#10063;</a>, <a href=''>C++ &#10063;</a>, <a href=''>Javascript &#10063;</a></li>
-        <li>Front-end: <a href=''>Javascript &#10063;</a>, <a href=''>React &#10063;</a>, CSS, HTML</li>
+        <li>Object-oriented: Proficient in Python and Javascript, moderately proficient in C++</li>
+        <li>Front-end Web Development: Proficient in Javascript, React.js, CSS and HTML</li>
        </ul>
        
-       <h2>Contact</h2>
+       <h2>Contact Bernard</h2>
        <ul>
-        <li>Email: bernardhmendez@protonmail.com</li>
-        <li>PGP: [Numbers]</li>
-        <li>Signal: 562 277 4954</li>
+        <li>You can contact Bernard at: bernardhmendez@gmail.com or bernardhmendez@protonmail.com</li>
        </ul>
        
      </div>
---
+++ src/homepage/Section.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './About.css';
+
+class Section extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        expanded: true,
+    }
+  }
+
+  collapse() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        tte
+      </div>
+    );
+  }
+}
+
+export default Section;
